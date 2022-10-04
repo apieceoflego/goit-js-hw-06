325e9df4c44ee4d6bdea32d9aef16e5b3e7b310e
apieceoflego <olehprychshepchuk@gmail.com>
2022-10-04 18:43:29 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const inputRef = document.querySelector('#font-size-control');
 const spanRef = document.querySelector('#text');
+spanRef.style.fontSize = inputRef.value + 'px';
 inputRef.addEventListener('input', event => {
   spanRef.style.fontSize = inputRef.value + 'px';
 });
